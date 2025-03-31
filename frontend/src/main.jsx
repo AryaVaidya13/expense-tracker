14427bd6797ce54d493cb0d76c93bda6ae93e95f
AryaVaidya13 <arya.vaidya59@gmail.com>
2025-03-31 14:24:40 +0530

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
-
+import { Provider } from "@chakra-ui/react"
+import { BrowserRouter } from "react-router-dom"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+        <Provider>
+          <App />
+        </Provider>
+      </BrowserRouter>
   </StrictMode>,
 )
